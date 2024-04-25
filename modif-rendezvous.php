96551804539da1f269568621e380c96d6100f106
MahaNalouti <164193343+MahaNalouti@users.noreply.github.com>
2024-04-26 00:03:49 +0100

    Update modif-rendezvous.php

--- modif-rendezvous.php
+++ modif-rendezvous.php
@@ -9,6 +9,7 @@ include("connection.php");
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
 	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="input.css">
 	<title>Modifier Rendez-vous</title>
 </head>
 <body>
